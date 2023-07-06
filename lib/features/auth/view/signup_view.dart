a9358fa87d84b4378ab06b5b1e7197eaa59009b0
CicakKayang <kgaluhr@gmail.com>
2023-07-06 20:36:02 +0700

    2 commit

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:coba1/common/common.dart';
-import 'package:coba1/common/loading_page.dart';
 import 'package:coba1/constants/constants.dart';
 import 'package:coba1/features/auth/controller/auth_controller.dart';
 import 'package:coba1/features/auth/view/login_view.dart';
